0c9aa459ad9d3e65d6ba6c12a462fa8b9b2db516
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-12-22 15:01:13 +0700

    [Added]: add comment of struct HTMLVariables

--- generate/generate.go
+++ generate/generate.go
@@ -5,13 +5,14 @@ import (
 	fakeWriter "play-golang/writer"
 )
 
+// HTMLValiables - pass variables to html template
 type HTMLValiables struct {
 	Text string
 }
 
 // NewHTML - pass html template and html valiable struct for generate new html template
 func NewHTML(htmlTemplate string, htmlValiables HTMLValiables) string {
-	readTemplate, _ := template.New("newtemplate").Parse(htmlTemplate)
+	readTemplate, _ := template.New("htmltemplate").Parse(htmlTemplate)
 	writer := fakeWriter.Writer{}
 	readTemplate.Execute(&writer, htmlValiables)
 	return string(writer.Data)
